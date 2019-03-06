5ad62d5a62b2aba12e7260855bfff629e2b195df
Jaffer <alijaffer3000@gmail.com>
2019-03-05 21:31:13 -0500

    framework

---
+++ identikit.js
@@ -0,0 +1,16 @@
+(function(window){
+    'use strict'
+    function init(){
+        var identikit = {
+        };
+
+        function identikit.render_simple_chart(container, data){ // container : div object to house chart, data: formatted dateset (watchtower form)
+
+        }
+
+        return identikit;
+    }
+    if(typeof(identikit) === 'undefined'){
+        window.identikit = init();
+    }
+})(window);
